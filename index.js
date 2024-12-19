5368a3ceb136b346a961cd6d351a889a099db3fa
24LP8255 24LP8255 <24LP8255@G301-CC0203-2.local>
2024-12-19 15:07:58 +0800

    1

--- index.js
+++ index.js
@@ -8,16 +8,20 @@ let highestScore = 0;
 const pointer = document.createElement("div");
 const livesCont = document.createElement("div")
 livesCont.classList.add("liveCont")
-const liveIcon1 = document.createElement("img")
-const liveIcon2 = document.createElement("img")
-const liveIcon3 = document.createElement("img")
+livesCont.id = "liveCont"
+const liveIcon1 = document.createElement("div")
+const liveIcon2 = document.createElement("div")
+const liveIcon3 = document.createElement("div")
 liveIcon1.classList.add("liveIcon")
 liveIcon2.classList.add("liveIcon")
 liveIcon3.classList.add("liveIcon")
 pointer.classList.add("pointer")
-document.getElementById("container").appendChild(pointer)
 pointer.id  = "pointer"
-
+document.getElementById("container").appendChild(pointer)
+document.getElementById("container").appendChild(livesCont)
+document.getElementById("liveCont").appendChild(liveIcon1);
+document.getElementById("liveCont").appendChild(liveIcon2);
+document.getElementById("liveCont").appendChild(liveIcon3);
 
 function score_u(){
     score+=1;
@@ -29,17 +33,18 @@ setInterval(score_u, 1000)
 function lives_u(){
     lives -=1;
     console.log(lives);
-    if (lives==3){
-        document.getElementById()
-    }else if (lives==2){
+    if (lives==2){
+        document.getElementById("liveCont").removeChild(liveIcon2);
         
     }else if (lives==1){
-
+        document.getElementById("liveCont").removeChild(liveIcon3)
+        ;
     }else if (lives == 0){
-        document.getElementById().
-        gameOver()
+        document.getElementById("liveCont").removeChild(liveIcon1)
+        gameOver();
+        
     }
 }
-
+setInterval(lives_u, 5000)
 
 // document.getElementById("pointer").innerHTML = "score:"+score_u(score)
