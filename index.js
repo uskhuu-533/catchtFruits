884d0bc92324e1a72afe8255d850dd6b90c6411d
uskhuu-533 <uskhuunymdavaa9@gmail.com>
2024-12-19 14:00:22 +0800

    gert

--- index.js
+++ index.js
@@ -6,9 +6,17 @@ let score = 0;
 let lives = 3;
 let highestScore = 0;
 const pointer = document.createElement("div");
+const livesCont = document.createElement("div")
+livesCont.classList.add("liveCont")
+const liveIcon1 = document.createElement("img")
+const liveIcon2 = document.createElement("img")
+const liveIcon3 = document.createElement("img")
+liveIcon1.classList.add("liveIcon")
+liveIcon2.classList.add("liveIcon")
+liveIcon3.classList.add("liveIcon")
 pointer.classList.add("pointer")
 document.getElementById("container").appendChild(pointer)
-pointer.id  = "pointer";
+pointer.id  = "pointer"
 
 
 function score_u(){
@@ -18,6 +26,20 @@ function score_u(){
 }
 
 setInterval(score_u, 1000)
+function lives_u(){
+    lives -=1;
+    console.log(lives);
+    if (lives==3){
+        document.getElementById()
+    }else if (lives==2){
+        
+    }else if (lives==1){
+
+    }else if (lives == 0){
+        document.getElementById().
+        gameOver()
+    }
+}
 
 
 // document.getElementById("pointer").innerHTML = "score:"+score_u(score)
